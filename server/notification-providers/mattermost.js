185611722ee92dce32de76d09bd2d4ddabc5de68
Cameron Maclean <34384474+BowsiePup@users.noreply.github.com>
2025-12-23 19:48:24 -0500

    Refactor Mattermost notification sending logic

--- server/notification-providers/mattermost.js
+++ server/notification-providers/mattermost.js
@@ -12,7 +12,6 @@ class Mattermost extends NotificationProvider {
         const okMsg = "Sent Successfully.";
 
         try {
-            let config = this.getAxiosConfigWithProxy({});
             const mattermostUserName = notification.mattermostusername || "Uptime Kuma";
             // If heartbeatJSON is null, assume non monitoring notification (Certificate warning) or testing.
             if (heartbeatJSON == null) {
@@ -20,12 +19,11 @@ class Mattermost extends NotificationProvider {
                     username: mattermostUserName,
                     text: msg,
                 };
-                await axios.post(notification.mattermostWebhookUrl, mattermostTestData, config);
+                await axios.post(notification.mattermostWebhookUrl, mattermostTestData);
                 return okMsg;
             }
 
             let mattermostChannel;
-
             if (typeof notification.mattermostchannel === "string") {
                 mattermostChannel = notification.mattermostchannel.toLowerCase();
             }
@@ -33,7 +31,6 @@ class Mattermost extends NotificationProvider {
             const mattermostIconEmoji = notification.mattermosticonemo;
             let mattermostIconEmojiOnline = "";
             let mattermostIconEmojiOffline = "";
-
             if (mattermostIconEmoji && typeof mattermostIconEmoji === "string") {
                 const emojiArray = mattermostIconEmoji.split(" ");
                 if (emojiArray.length >= 2) {
@@ -41,70 +38,132 @@ class Mattermost extends NotificationProvider {
                     mattermostIconEmojiOffline = emojiArray[1];
                 }
             }
+
             const mattermostIconUrl = notification.mattermosticonurl;
             let iconEmoji = mattermostIconEmoji;
-            let statusField = {
-                short: false,
-                title: "Error",
-                value: heartbeatJSON.msg,
-            };
-            let statusText = "unknown";
-            let color = "#000000";
-            if (heartbeatJSON.status === DOWN) {
-                iconEmoji = mattermostIconEmojiOffline || mattermostIconEmoji;
-                statusField = {
-                    short: false,
-                    title: "Error",
-                    value: heartbeatJSON.msg,
-                };
-                statusText = "down.";
-                color = "#FF0000";
-            } else if (heartbeatJSON.status === UP) {
-                iconEmoji = mattermostIconEmojiOnline || mattermostIconEmoji;
-                statusField = {
-                    short: false,
-                    title: "Ping",
-                    value: heartbeatJSON.ping + "ms",
-                };
-                statusText = "up!";
-                color = "#32CD32";
-            }
 
-            let mattermostdata = {
-                username: monitorJSON.name + " " + mattermostUserName,
+            if (heartbeatJSON.status === DOWN) {
+                let mattermostdowndata = {
+                    username: mattermostUserName,
+                    channel: mattermostChannel,
+                    icon_emoji: iconEmoji,
+                    icon_url: mattermostIconUrl,
+                    attachments: [
+                        {
+                            fallback: mattermostIconEmojiOffline + " Your service " + monitorJSON.name + " went down.",
+                            color: "#FF0000",
+                            title: "❌ Your service " + monitorJSON.name + " went down. ❌",
+                            title_link: monitorJSON.url,
+                            fields: [
+                                {
+                                    short: true,
+                                    title: "Service Name",
+                                    value: monitorJSON.name,
+                                },
+                                {
+                                    short: true,
+                                    title: monitorJSON.type === "push" ? "Service Type" : "Service URL",
+                                    value: this.extractAddress(monitorJSON),
+                                },
+                                {
+                                    short: false,
+                                    title: `Time (${heartbeatJSON.timezone})`,
+                                    value: heartbeatJSON.localDateTime,
+                                },
+                                {
+                                    short: false,
+                                    title: "Error",
+                                    value: heartbeatJSON.msg == null ? "N/A" : heartbeatJSON.msg,
+                                },
+                            ],
+                        }
+                    ]
+                };
+                await axios.post(notification.mattermostWebhookUrl, mattermostdowndata);
+                return okMsg;
+            } else if (heartbeatJSON.status === UP) {
+                let mattermostupdata = {
+                    username: mattermostUserName,
+                    channel: mattermostChannel,
+                    icon_emoji: iconEmoji,
+                    icon_url: mattermostIconUrl,
+                    attachments: [
+                        {
+                            fallback: mattermostIconEmojiOnline + " Your service " + monitorJSON.name + " is up!",
+                            color: "#32CD32",
+                            title: "✅ Your service " + monitorJSON.name + " is up! ✅",
+                            title_link: monitorJSON.url,
+                            fields: [
+                                {
+                                    short: true,
+                                    title: "Service Name",
+                                    value: monitorJSON.name,
+                                },
+                                {
+                                    short: true,
+                                    title: monitorJSON.type === "push" ? "Service Type" : "Service URL",
+                                    value: this.extractAddress(monitorJSON),
+                                },
+                                {
+                                    short: false,
+                                    title: `Time (${heartbeatJSON.timezone})`,
+                                    value: heartbeatJSON.localDateTime,
+                                },
+                                {
+                                    short: false,
+                                    title: "Ping",
+                                    value: heartbeatJSON.ping == null ? "N/A" : heartbeatJSON.ping + " ms",
+                                },
+                            ],
+                        }
+                    ]
+                };
+                await axios.post(notification.mattermostWebhookUrl, mattermostupdata);
+                return okMsg;
+            }
+            // If we reach here, it means the status is unknown.
+
+            let mattermostUnknownData = {
+                username: mattermostUserName,
                 channel: mattermostChannel,
                 icon_emoji: iconEmoji,
                 icon_url: mattermostIconUrl,
                 attachments: [
                     {
-                        fallback:
-                            "Your " +
-                            monitorJSON.pathName +
-                            " service went " +
-                            statusText,
-                        color: color,
-                        title:
-                            monitorJSON.pathName +
-                            " service went " +
-                            statusText,
+                        fallback: mattermostIconEmoji + " Your service " + monitorJSON.name + " status is unknown.",
+                        color: "#000000",
+                        title: "Your service " + monitorJSON.name + " status is unknown.",
                         title_link: monitorJSON.url,
                         fields: [
-                            statusField,
                             {
                                 short: true,
-                                title: `Time (${heartbeatJSON["timezone"]})`,
+                                title: "Service Name",
+                                value: monitorJSON.name,
+                            },
+                            {
+                                short: true,
+                                title: "Service Type",
+                                value: monitorJSON.type === "push" ? "Service Type" : "Service URL",
+                            },
+                            {
+                                short: false,
+                                title: `Time (${heartbeatJSON.timezone})`,
                                 value: heartbeatJSON.localDateTime,
                             },
+                            {
+                                short: false,
+                                title: "Error",
+                                value: heartbeatJSON.msg == null ? "N/A" : heartbeatJSON.msg,
+                            },
                         ],
-                    },
-                ],
+                    }
+                ]
             };
-            await axios.post(notification.mattermostWebhookUrl, mattermostdata, config);
+            await axios.post(notification.mattermostWebhookUrl, mattermostUnknownData);
             return okMsg;
         } catch (error) {
             this.throwGeneralAxiosError(error);
         }
-
     }
 }
 
